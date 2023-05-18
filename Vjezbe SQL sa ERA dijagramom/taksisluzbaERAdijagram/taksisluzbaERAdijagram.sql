3a9f03613ae03cf588a155efde90133ce8aef563
mijatovicvalentin <mijatovic.valentin123@gmail.com>
2023-05-18 23:21:12 +0200

    Update taksisluzbaERAdijagram.sql

--- Vjezbe SQL sa ERA dijagramom/taksisluzbaERAdijagram/taksisluzbaERAdijagram.sql
+++ Vjezbe SQL sa ERA dijagramom/taksisluzbaERAdijagram/taksisluzbaERAdijagram.sql
@@ -11,7 +11,7 @@ create table vozilo(
 	id int not null primary key identity(1,1),
 	cijena decimal(18,2),
 	marka  varchar(50),
-	godi�te varchar(50),
+	godište varchar(50),
 );
 
 
@@ -50,3 +50,36 @@ alter table voznja_putnik add foreign key (voznja) references voznja(id);
 alter table voznja_putnik add foreign key (putnik) references putnik(id);
 alter table vozac add foreign key (vozilo) references vozilo(id);
 alter table voznja add foreign key (vozac) references vozac(id);
+
+
+
+insert into vozilo (cijena,marka,godište) values('20000','AUDI','2018');
+insert into vozilo (cijena,marka,godište) values('18000','BMW','2016');
+insert into vozilo (cijena,marka,godište) values('14000','GOLFIV','2014');
+insert into vozilo (cijena,marka,godište) values('16000','AUDI8','2018');
+
+
+insert into putnik (ime,prezime,oib) values('luka','lukic','98373857689');
+insert into putnik (ime,prezime,oib) values('petar','juros','98375657689');
+insert into putnik (ime,prezime,oib) values('jan','luvic','98396757689');
+insert into putnik (ime,prezime,oib) values('karlo','majic','98309277689');
+
+
+insert into vozac (ime,prezime,oib,vozilo) values('marko','keric','09687654951','AUDI');
+insert into vozac (ime,prezime,oib,vozilo) values('marin','banfic','98692645761','BMW');
+insert into vozac (ime,prezime,oib,vozilo) values('kruno','lukic','98690895661','GOLFIV');
+insert into vozac (ime,prezime,oib,vozilo) values('luka','petric','23490485761','AUDI8');
+
+
+insert into voznja (kilometraza,cijena,potrosnja,vozac) values('6044','24','8','marko');
+insert into voznja (kilometraza,cijena,potrosnja,vozac) values('243','24','8','marin');
+insert into voznja (kilometraza,cijena,potrosnja,vozac) values('380','24','8','kruno');
+insert into voznja (kilometraza,cijena,potrosnja,vozac) values('8400','24','8','luka');
+
+
+insert into voznja_putnik (voznja,putnik) values('voznja','putnik');
+insert into voznja_putnik (voznja,putnik) values('voznja','putnik');
+insert into voznja_putnik (voznja,putnik) values('voznja','putnik');
+insert into voznja_putnik (voznja,putnik) values('voznja','putnik');
+
+

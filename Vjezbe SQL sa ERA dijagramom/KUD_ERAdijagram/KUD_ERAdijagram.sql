e460a3754c77edfc1f24084344622a8f84ccb2d9
mijatovicvalentin <mijatovic.valentin123@gmail.com>
2023-05-19 14:16:41 +0200

    Update KUD_ERAdijagram.sql inserti

--- Vjezbe SQL sa ERA dijagramom/KUD_ERAdijagram/KUD_ERAdijagram.sql
+++ Vjezbe SQL sa ERA dijagramom/KUD_ERAdijagram/KUD_ERAdijagram.sql
@@ -40,3 +40,28 @@ create table mjesto(
 alter table clan_nastup add foreign key (clan) references clan(id);
 alter table clan_nastup add foreign key (nastup) references nastup(id);
 alter table mjesto add foreign key (nastup) references nastup(id);
+
+
+insert into clan (ime,prezime,oib) values('petar','lukic','75684958671');
+insert into clan (ime,prezime,oib) values('martin','peric','67584958671');
+insert into clan (ime,prezime,oib) values('maja','basta','09867958671');
+insert into clan (ime,prezime,oib) values('ivana','juros','09584987671');
+
+
+insert into nastup (adresa,naziv) values ('1','2');
+insert into nastup (adresa,naziv) values ('2','3');
+insert into nastup (adresa,naziv) values ('3','4');
+insert into nastup (adresa,naziv) values ('4','5');
+
+
+
+insert into mjesto (adresa,nastup) values('jozeivekica21','labudjejezero');
+insert into mjesto (adresa,nastup) values('petrapreadovica1','vinkovackejeseni');
+insert into mjesto (adresa,nastup) values('petrapreadovica1','kikijevples');
+insert into mjesto (adresa,nastup) values('bajelovinog4','morskifestival');
+
+
+insert into clan_nastup (clan,nastup) values('1','2');
+insert into clan_nastup (clan,nastup) values('2','3');
+insert into clan_nastup (clan,nastup) values('3','4');
+insert into clan_nastup (clan,nastup) values('4','5');

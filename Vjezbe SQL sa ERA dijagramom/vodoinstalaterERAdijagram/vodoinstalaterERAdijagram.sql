b80348eefa31c333888df68ae71018af1c7a5203
mijatovicvalentin <mijatovic.valentin123@gmail.com>
2023-05-19 16:02:02 +0200

    Update vodoinstalaterERAdijagram.sql inserti

--- Vjezbe SQL sa ERA dijagramom/vodoinstalaterERAdijagram/vodoinstalaterERAdijagram.sql
+++ Vjezbe SQL sa ERA dijagramom/vodoinstalaterERAdijagram/vodoinstalaterERAdijagram.sql
@@ -54,3 +54,30 @@ alter table popravak_kvar add foreign key (popravak) references popravak(id);
 alter table popravak_kvar add foreign key (kvar) references kvar(id);
 alter table zaposlenik add foreign key (strucnasprema)references strucnasprema(id);
 alter table popravak add foreign key (zaposlenik) references zaposlenik(id);
+
+insert into kvar(cijena,naziv,kolicina) values('15.26','senzor','5');
+insert into kvar(cijena,naziv,kolicina) values('16.26','svjecica','3');
+insert into kvar(cijena,naziv,kolicina) values('18.26','disk','1');
+insert into kvar(cijena,naziv,kolicina) values('13.26','filter','2');
+
+
+insert into strucnasprema(naziv) values('vodoinstalater');
+insert into strucnasprema(naziv) values('vodoinstalater');
+insert into strucnasprema(naziv) values('vodoinstalater');
+insert into strucnasprema(naziv) values('vodoinstalater');
+
+insert into zaposlenik (ime,prezime,oib,strucnasprema) values('luka','peric','89076549817','1')
+insert into zaposlenik (ime,prezime,oib,strucnasprema) values('ivan','ivanusec','95076349817','1')
+insert into zaposlenik (ime,prezime,oib,strucnasprema) values('mario','jokic','89056549817','1')
+insert into zaposlenik (ime,prezime,oib,strucnasprema) values('ena','maric','89087169817','1')
+
+
+insert into popravak (cijena,trajanje,naziv,zaposlenik) values('45.86','1','izmjena','1');
+insert into popravak (cijena,trajanje,naziv,zaposlenik) values('23.45','1','promjena','1');
+insert into popravak (cijena,trajanje,naziv,zaposlenik) values('16.36','1','zamjena','1');
+insert into popravak (cijena,trajanje,naziv,zaposlenik) values('32.78','1','razmjena','1');
+
+insert into popravak_kvar (popravak,kvar) values('1','2');
+insert into popravak_kvar (popravak,kvar) values('1','2');
+insert into popravak_kvar (popravak,kvar) values('1','2');
+insert into popravak_kvar (popravak,kvar) values('1','2');

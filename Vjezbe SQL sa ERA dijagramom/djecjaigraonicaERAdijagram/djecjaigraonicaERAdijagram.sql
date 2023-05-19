de24ef10679786875beb8fe2bc0c74328fbef6b3
mijatovicvalentin <mijatovic.valentin123@gmail.com>
2023-05-19 12:12:34 +0200

    Update djecjaigraonicaERAdijagram.sql inserti

--- Vjezbe SQL sa ERA dijagramom/djecjaigraonicaERAdijagram/djecjaigraonicaERAdijagram.sql
+++ Vjezbe SQL sa ERA dijagramom/djecjaigraonicaERAdijagram/djecjaigraonicaERAdijagram.sql
@@ -42,3 +42,29 @@ alter table dijete_skupina add foreign key (dijete) references dijete(id);
 alter table dijete_skupina add foreign key (skupina) references skupina(id);
 alter table skupina add foreign key (teta) references teta(id);
 
+
+insert into dijete (ime,prezime,spol,razred)  values('luka','birtic','1','6');
+insert into dijete (ime,prezime,spol,razred)  values('marija','lukic','2','5');
+insert into dijete (ime,prezime,spol,razred)  values('ivan','jukic','1','6');
+insert into dijete (ime,prezime,spol,razred)  values('jan','juros','1','8');
+
+
+
+insert into teta (ime,prezime,oib) values('ana'.'birkic','96863904378');
+insert into teta (ime,prezime,oib) values('petra'.'martic','96863904378');
+insert into teta (ime,prezime,oib) values('marija'.'majic','85948576983');
+insert into teta (ime,prezime,oib) values('zola'.'nikic','18948596079');
+
+
+
+insert into skupina (naziv,broj,teta) values('liljan','4','ana');
+insert into skupina (naziv,broj,teta) values('pcelica','3','petra');
+insert into skupina (naziv,broj,teta) values('list','1','marija');
+insert into skupina (naziv,broj,teta) values('cvijet','2','zola');
+
+
+
+insert into dijete_skupina (dijete,skupina) values('luka','liljan');
+insert into dijete_skupina (dijete,skupina) values('marija','pcelica');
+insert into dijete_skupina (dijete,skupina) values('ivan','list');
+insert into dijete_skupina (dijete,skupina) values('jan','cvijet');

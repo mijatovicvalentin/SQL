2b47936ed74c177b2b2a81e9388bb46be1a691be
mijatovicvalentin <mijatovic.valentin123@gmail.com>
2023-05-20 16:50:56 +0200

    Update isvu.sql inserti

--- Vjezbe SQL sa ERA dijagramom/isvuERAdijagram/isvu.sql
+++ Vjezbe SQL sa ERA dijagramom/isvuERAdijagram/isvu.sql
@@ -29,7 +29,7 @@ id int not null primary key identity(1,1),
 student int not null,
 course int not null,
 academicyear int not null,
-dateofenrollement int not null,
+dateofenrollement datetime,
 );
 create table academicyear(
 id int not null primary key identity(1,1),
@@ -49,3 +49,33 @@ alter table student_course add foreign key (student) references student(id);
 alter table student_course add foreign key (course) references course(id);
 alter table student_course add foreign key (academicyear) references academicyear(id);
 alter table academicyear add foreign key (semester) references semester(id);
+
+
+insert into student (firstname,lastname,jmbag) values('mark','cofh','7285738471');
+insert into student (firstname,lastname,jmbag) values('raven','reyes','9485738471');
+insert into student (firstname,lastname,jmbag) values('john','tony','9472948716');
+insert into student (firstname,lastname,jmbag) values('layla','murhpy','9488758471');
+
+insert into course (name,ects,description) values('mechanic','ten','mechanic');
+insert into course (name,ects,description) values('physics','eight','mechanic');
+insert into course (name,ects,description) values('robotics','six','mechanic');
+insert into course (name,ects,description) values('chemistry','seven','mechanic');
+
+
+insert into semester (academicyear) values('1');
+insert into semester (academicyear) values('1');
+insert into semester (academicyear) values('1');
+insert into semester (academicyear) values('1');
+
+
+insert into academicyear (name,startdate,enddate,semester) values('firstyear','october','december','1');
+insert into academicyear (name,startdate,enddate,semester) values('secondyear','november','july','1');
+insert into academicyear (name,startdate,enddate,semester) values('thirdyear','may','december','1');
+insert into academicyear (name,startdate,enddate,semester) values('fourthyear','october','june','1');
+
+
+
+insert into student_course (student,course,academicyear,dateofenrollement) values('1','2','3','2023-04-14');
+insert into student_course (student,course,academicyear,dateofenrollement) values('1','2','3','2023-04-14');
+insert into student_course (student,course,academicyear,dateofenrollement) values('1','2','3','2023-04-14');
+insert into student_course (student,course,academicyear,dateofenrollement) values('1','2','3','2023-04-14');

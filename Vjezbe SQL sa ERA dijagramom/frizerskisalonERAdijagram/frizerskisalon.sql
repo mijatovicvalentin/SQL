9a142b11da5d758a8e5187fa429d5d49c116ad4b
mijatovicvalentin <mijatovic.valentin123@gmail.com>
2023-05-20 13:13:02 +0200

    Update frizerskisalon.sql inserti

--- Vjezbe SQL sa ERA dijagramom/frizerskisalonERAdijagram/frizerskisalon.sql
+++ Vjezbe SQL sa ERA dijagramom/frizerskisalonERAdijagram/frizerskisalon.sql
@@ -44,3 +44,28 @@ create table strucnasprema(
 alter table usluga add foreign key (korisnik) references korisnik(id);
 alter table djelatnica add foreign key (korisnik) references korisnik(id);
 alter table strucnasprema add foreign key (djelatnica) references djelatnica(id);
+
+
+insert into korisnik (ime,prezime,oib,spol) values('luka','kolak','09687536521','1');
+insert into korisnik (ime,prezime,oib,spol) values('marija','pistic','09574937613','2');
+insert into korisnik (ime,prezime,oib,spol) values('kruno','bojic','09263958471','1');
+insert into korisnik (ime,prezime,oib,spol) values('tea','karic','09583958761','2');
+
+
+
+insert into djelatnica ( ime,prezime,oib,korisnik) values('petra','majic','09574839871','1');
+insert into djelatnica ( ime,prezime,oib,korisnik) values('zola','delic','09867839871','1');
+insert into djelatnica ( ime,prezime,oib,korisnik) values('vanja','kisic','98765839871','1');
+insert into djelatnica ( ime,prezime,oib,korisnik) values('jela','koric','09576453217','1');
+
+
+insert into strucnasprema (djelatnica) values('1')
+insert into strucnasprema (djelatnica) values('1')
+insert into strucnasprema (djelatnica) values('1')
+insert into strucnasprema (djelatnica) values('1')
+
+
+insert into usluga (korisnik,trajanje,cijena,proizvod) values('1','15','26.80','crvenaboja');
+insert into usluga (korisnik,trajanje,cijena,proizvod) values('1','30','24.80','plavaboja');
+insert into usluga (korisnik,trajanje,cijena,proizvod) values('1','35','30.80','zelenaboja');
+insert into usluga (korisnik,trajanje,cijena,proizvod) values('1','28','18.80','sivaboja');

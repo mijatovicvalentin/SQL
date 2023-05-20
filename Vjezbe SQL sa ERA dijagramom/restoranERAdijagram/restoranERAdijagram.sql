f677622e90814597cee731015d41d51905b3f834
mijatovicvalentin <mijatovic.valentin123@gmail.com>
2023-05-20 18:05:41 +0200

    Update restoranERAdijagram.sql inserti

--- Vjezbe SQL sa ERA dijagramom/restoranERAdijagram/restoranERAdijagram.sql
+++ Vjezbe SQL sa ERA dijagramom/restoranERAdijagram/restoranERAdijagram.sql
@@ -35,3 +35,32 @@ create table jelo_pice(
 alter table jelo_pice add foreign key (jelo) references jelo(id);
 alter table jelo_pice add foreign key (pice) references pice(id);
 alter table jelo add foreign key (kategorija) references kategorija(id);
+
+
+
+insert into  pice (naziv,cijena) values('mojito','10');
+insert into  pice (naziv,cijena) values('sprinksten','24');
+insert into  pice (naziv,cijena) values('vino','16');
+insert into  pice (naziv,cijena) values('brahler','12');
+
+
+insert into kategorija (brojkategorija) values('4');
+insert into kategorija (brojkategorija) values('2');
+insert into kategorija (brojkategorija) values('2');
+insert into kategorija (brojkategorija) values('4');
+
+
+insert into jelo (cijena,naziv,kategorija) values('18.26','bakalar','1');
+insert into jelo (cijena,naziv,kategorija) values('12.26','stuka','1');
+insert into jelo (cijena,naziv,kategorija) values('24.26','brancin','1');
+insert into jelo (cijena,naziv,kategorija) values('1.26','krompir','1');
+
+
+insert into  jelo_pice (jelo,pice) values('1','2');
+insert into  jelo_pice (jelo,pice) values('1','2');
+insert into  jelo_pice (jelo,pice) values('1','2');
+insert into  jelo_pice (jelo,pice) values('1','2');
+
+
+
+

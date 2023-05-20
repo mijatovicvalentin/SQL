fe12fc96d926bfbee05af036deeb0c88958bca46
mijatovicvalentin <mijatovic.valentin123@gmail.com>
2023-05-20 17:01:02 +0200

    Update klubcitateljaERAdijagram.sql inserti

--- Vjezbe SQL sa ERA dijagramom/klubcitatelja.sql ERA dijagram/klubcitateljaERAdijagram.sql
+++ Vjezbe SQL sa ERA dijagramom/klubcitatelja.sql ERA dijagram/klubcitateljaERAdijagram.sql
@@ -34,3 +34,21 @@ create table posudba(
 
 alter table posudba add foreign key (citatelj) references citatelj(sifra);
 alter table knjiga add foreign key (posudba) references posudba(sifra);
+
+
+
+insert into	 citatelj (knjiga,ime,prezime,kartica,clanstvo) values('1', 'mario','jokic','karticajedan','clankluba');
+insert into	 citatelj (knjiga,ime,prezime,kartica,clanstvo) values('1', 'marija','boras','karticajedan','clankluba');
+insert into	 citatelj (knjiga,ime,prezime,kartica,clanstvo) values('1', 'petar','peran','karticajedan','clankluba');
+insert into	 citatelj (knjiga,ime,prezime,kartica,clanstvo) values('1', 'jan','krlezic','karticajedan','clankluba');
+
+insert into posudba (trajanje,citatelj,oznaka) values('30','1','oznakabrojtri');
+insert into posudba (trajanje,citatelj,oznaka) values('30','2','oznakabrojdvanaest');
+insert into posudba (trajanje,citatelj,oznaka) values('30','3','oznakabrojjedan');
+insert into posudba (trajanje,citatelj,oznaka) values('30','4','oznakabrojsedam');
+
+
+insert into knjiga (citatelj,posudba,naziv,oznaka,imeautora) values('1','2','mocniljudi','oznakabrojtri','marisel');
+insert into knjiga (citatelj,posudba,naziv,oznaka,imeautora) values('1','2','sumrak','oznakabrojdvanaest','lokhart');
+insert into knjiga (citatelj,posudba,naziv,oznaka,imeautora) values('1','2','nocneptice','oznakabrojjedan','joiole');
+insert into knjiga (citatelj,posudba,naziv,oznaka,imeautora) values('1','2','lukavac','oznakabrojsedam','koionart');

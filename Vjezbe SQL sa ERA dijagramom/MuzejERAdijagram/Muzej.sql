5d9c1f8058509df9443252ff80032ca42bd0ac32
mijatovicvalentin <mijatovic.valentin123@gmail.com>
2023-05-20 18:39:13 +0200

    Update Muzej.sql inserti

--- Vjezbe SQL sa ERA dijagramom/MuzejERAdijagram/Muzej.sql
+++ Vjezbe SQL sa ERA dijagramom/MuzejERAdijagram/Muzej.sql
@@ -2,81 +2,64 @@ use master;
 
 drop database muzej;
 
-create database muzej;
 
-use  muzej;
+create database  muzej;
 
+use muzej;
 
-create table muzej(
-		sifra int not null primary key identity(1,1),
-		izlozba int,
-		adresa varchar(50),
-		naziv  varchar(50),
-		email  varchar(50),
-);
 
 create table izlozba(
-		sifra int not null primary key identity(1,1),
-		clan int,
-		naziv  varchar(50),
-		atrij  varchar(50),
-		trajanje  int,
+	id int not null primary key identity(1,1),
+	djelo int not null,
+	kustos int not null,
+	trajanje int,
 );
 
+
+create table djelo(
+	id int not null primary key identity(1,1),
+	naziv varchar(50),
+	vrijednost decimal(18,2),
+);
 
 create table kustos(
-		sifra int not null primary key identity(1,1),
-		ime varchar(50),
-		prezime varchar(50),
-		oib char(11),
+	id int not null primary key identity(1,1),
+	ime varchar(50) not null,
+	prezime varchar(50) not null,
 );
-
 
 create table sponzor(
-		sifra int not null primary key identity(1,1),
-		ime varchar(50),
-		prezime varchar(50),
-		posjetnica varchar(50),
+	id int not null primary key identity(1,1),
+	ime varchar(50),
+	prezime varchar(50),
+	posjetnica varchar(50),
+	izlozba int not null,
 );
 
 
-create table clan(
-		sifra int not null primary key identity(1,1),
-		kustos int,
-		sponzor int,
-);
 
-alter table muzej add foreign key (izlozba) references izlozba(sifra);
-alter table izlozba add foreign key (clan) references clan(sifra);
-alter table clan add foreign key (kustos) references kustos(sifra);
-alter table clan add foreign key (sponzor) references sponzor(sifra);
+alter table izlozba add foreign key (djelo) references djelo(id);
+alter table izlozba add foreign key (kustos) references kustos(id);
+alter table sponzor add foreign key (izlozba) references izlozba(id);
 
 
-insert into kustos (ime,prezime,oib) values('luka','maric','08976387621')
-insert into kustos (ime,prezime,oib) values('petar','lukic','85766387621')
-insert into kustos (ime,prezime,oib) values('branko','baros','09876387621')
-insert into kustos (ime,prezime,oib) values('marin','varos','08976387621')
+insert into djelo (naziv,vrijednost) values('monaliza','286.000');
+insert into djelo (naziv,vrijednost) values('ludbreg','246.000');
+insert into djelo (naziv,vrijednost) values('osaka','16.000');
+insert into djelo (naziv,vrijednost) values('iatomonto','28.000');
 
-insert into sponzor (ime,prezime,posjetnica) values('luka','majic','majicluka')
-insert into sponzor (ime,prezime,posjetnica) values('ivan','lukas','ivanlukas')
-insert into sponzor (ime,prezime,posjetnica) values('lukas','ivic','lukasivic')
-insert into sponzor (ime,prezime,posjetnica) values('mario','dvorski','mariodvorski')
+insert into kustos (ime,prezime) values('luka','rabec');
+insert into kustos (ime,prezime) values('marin','vili');
+insert into kustos (ime,prezime) values('marina','horvat');
+insert into kustos (ime,prezime) values('matea','lokac');
 
-
-insert into clan (sponzor,kustos) values('1','2')
-insert into clan (sponzor,kustos) values('1','2')
-insert into clan (sponzor,kustos) values('1','2')
-insert into clan (sponzor,kustos) values('1','2')
-
+insert into izlozba (djelo,kustos,trajanje) values('1','2','80');
+insert into izlozba (djelo,kustos,trajanje) values('1','2','60');
+insert into izlozba (djelo,kustos,trajanje) values('1','2','120');
+insert into izlozba (djelo,kustos,trajanje) values('1','2','120');
 
 
-insert into izlozba (clan,naziv,atrij,trajanje) values('1','modernost','dva','30')
-insert into izlozba (clan,naziv,atrij,trajanje) values('1','zivot','sest','30')
-insert into izlozba (clan,naziv,atrij,trajanje) values('1','podjela','cetiri','45')
-insert into izlozba (clan,naziv,atrij,trajanje) values('1','vijek','jedan','35')
-
-
-insert into muzej (izlozba,adresa,naziv,email) values('1','preradoviceva15','groteska','groteska@gmail.com')
-insert into muzej (izlozba,adresa,naziv,email) values('1','martinceva11','okulus','okulus@gmail.com')
-insert into muzej (izlozba,adresa,naziv,email) values('1','radiceva18','duhovi','duhovi@gmail.com')
-insert into muzej (izlozba,adresa,naziv,email) values('1','ulicakapetana12','umjetnost','umjetnost@gmail.com')
+insert into sponzor (ime,prezime,posjetnica,izlozba) values('marin','krojac','marinkrojac','1');
+insert into sponzor (ime,prezime,posjetnica,izlozba) values('lejla','baric','lejlabaric','1');
+insert into sponzor (ime,prezime,posjetnica,izlozba) values('karlo','dokatic','karlodokatic','1');
+insert into sponzor (ime,prezime,posjetnica,izlozba) values('ivana','vers','ivanavers','1');

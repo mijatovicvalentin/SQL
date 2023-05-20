18c2eeb49ab9307d8003b0a05c148c71e8b8d067
mijatovicvalentin <mijatovic.valentin123@gmail.com>
2023-05-20 17:41:01 +0200

    Update osnovnaskolaERAdijagram.sql inserti

--- Vjezbe SQL sa ERA dijagramom/osnovnaskolaERAdijagram/osnovnaskolaERAdijagram.sql
+++ Vjezbe SQL sa ERA dijagramom/osnovnaskolaERAdijagram/osnovnaskolaERAdijagram.sql
@@ -40,3 +40,25 @@ alter table dijete_radionica add foreign key (dijete) references dijete(id);
 alter table dijete_radionica add foreign key (radionica) references radionica(id);
 alter table radionica add foreign key (uciteljica) references uciteljica(id);
   
+  
+  
+insert into dijete (ime,prezime,razred) values('marin','lukas','prvi');
+insert into dijete (ime,prezime,razred) values('lea','bajs','drugi');
+insert into dijete (ime,prezime,razred) values('helena','jelas','prvi');
+insert into dijete (ime,prezime,razred) values('magda','jukic','drugi');
+
+
+insert into  uciteljica (radionica,ime,prezime,predmetpredavanja) values('1','marija','peric','hrvatski');
+insert into  uciteljica (radionica,ime,prezime,predmetpredavanja) values('1','leonarda','bajs','engleski');
+insert into  uciteljica (radionica,ime,prezime,predmetpredavanja) values('1','ivana','ljubic','matematika');
+insert into  uciteljica (radionica,ime,prezime,predmetpredavanja) values('1','nevenka','atrojic','prirodaidrustvo');
+
+insert into radionica (uciteljica,naziv) values('1','leptiri');
+insert into radionica (uciteljica,naziv) values('2','kolovoz');
+insert into radionica (uciteljica,naziv) values('3','matematika1');
+insert into radionica (uciteljica,naziv) values('4','priroda'); 
+
+insert into dijete_radionica (dijete,radionica) values('1','2');
+insert into dijete_radionica (dijete,radionica) values('1','2');
+insert into dijete_radionica (dijete,radionica) values('1','2');
+insert into dijete_radionica (dijete,radionica) values('1','2');

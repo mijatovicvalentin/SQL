2514ad7513da39e5760585ce3acb49749ef1d209
mijatovicvalentin <mijatovic.valentin123@gmail.com>
2023-05-20 17:12:52 +0200

    Update nakladnikERAdijagram.sql inserti

--- Vjezbe SQL sa ERA dijagramom/nakladnikERAdijagram/nakladnikERAdijagram.sql
+++ Vjezbe SQL sa ERA dijagramom/nakladnikERAdijagram/nakladnikERAdijagram.sql
@@ -36,9 +36,32 @@ create table mjesto(
 	id int not null primary key identity(1,1),
 	nakladnik int not null,
 	adresa varchar(50) not null,
-	naziv int,
+	naziv varchar(50),
 );
 
 alter table knjiga add foreign key (nakladnik) references nakladnik(id);
 alter table knjiga add foreign key (djelo) references djelo(id);
 alter table mjesto add foreign key (nakladnik) references nakladnik(id);
+
+
+
+insert into nakladnik (ime,prezime,oib,izdavackakuca) values('teodor','lukasov','94858674819','krompirici');
+insert into nakladnik (ime,prezime,oib,izdavackakuca) values('branimir','korijenovic','82938476981','liliputanci');
+insert into nakladnik (ime,prezime,oib,izdavackakuca) values('luka','marko','94898474819','rowrecords');
+insert into nakladnik (ime,prezime,oib,izdavackakuca) values('ernest','brajic','99847584819','rekordi');
+
+insert into djelo (naziv,autor,bestseller) values('sjecanje','teodor','1');
+insert into djelo (naziv,autor,bestseller) values('popusti','branimir','2');
+insert into djelo (naziv,autor,bestseller) values('odlazak','luka','1');
+insert into djelo (naziv,autor,bestseller) values('povratak','ernest','2');
+
+insert into  knjiga (nakladnik,djelo) values('1','2');
+insert into  knjiga (nakladnik,djelo) values('1','2');
+insert into  knjiga (nakladnik,djelo) values('1','2');
+insert into  knjiga (nakladnik,djelo) values('1','2');
+
+insert into mjesto (nakladnik,adresa,naziv) values('1','kraljatomislava6','osijek');
+insert into mjesto (nakladnik,adresa,naziv) values('2','branimira6','zagreb');
+insert into mjesto (nakladnik,adresa,naziv) values('3','kraljatvrtka18','petrinja');
+insert into mjesto (nakladnik,adresa,naziv) values('4','preradoviceva11','osijek');
+

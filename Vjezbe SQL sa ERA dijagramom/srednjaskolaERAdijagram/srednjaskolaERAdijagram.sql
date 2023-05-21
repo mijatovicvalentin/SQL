fe6bdc9898de30e86d3b0dfa87358b954bcf4a4f
mijatovicvalentin <mijatovic.valentin123@gmail.com>
2023-05-21 15:51:29 +0200

    Update srednjaskolaERAdijagram.sql inserti

--- Vjezbe SQL sa ERA dijagramom/srednjaskolaERAdijagram/srednjaskolaERAdijagram.sql
+++ Vjezbe SQL sa ERA dijagramom/srednjaskolaERAdijagram/srednjaskolaERAdijagram.sql
@@ -37,3 +37,24 @@ alter table profesor_razred add foreign key (profesor) references profesor(id);
 alter table profesor_razred add foreign key (razred) references razred(id);
 alter table ucenik add foreign key (razred) references razred(id);
 
+
+
+insert into razred (naziv) values('petib');
+insert into razred (naziv) values('drugia');
+insert into razred (naziv) values('trecia');
+insert into razred (naziv) values('petib');
+
+insert into profesor (ime,prezime,predmet) values('marina','vlasic','kemija');
+insert into profesor (ime,prezime,predmet) values('petar','boksic','fizika');
+insert into profesor (ime,prezime,predmet) values('stjepan','vlasic','matematika');
+insert into profesor (ime,prezime,predmet) values('luka','ivsic','stroajrstvo');
+
+insert into profesor_razred (profesor,razred) values('1','2');
+insert into profesor_razred (profesor,razred) values('1','2');
+insert into profesor_razred (profesor,razred) values('1','2');
+insert into profesor_razred (profesor,razred) values('1','2');
+
+insert into ucenik (ime,prezime,razred) values('kruno','boksic','1');
+insert into ucenik (ime,prezime,razred) values('marija','boksic','1');
+insert into ucenik (ime,prezime,razred) values('lena','ivesic','1');
+insert into ucenik (ime,prezime,razred) values('kruno','leko','1');

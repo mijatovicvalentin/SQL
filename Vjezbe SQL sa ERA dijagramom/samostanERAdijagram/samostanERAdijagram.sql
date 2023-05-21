e0030ee5d2b2587e06b37a8791ea37ebb4073e01
mijatovicvalentin <mijatovic.valentin123@gmail.com>
2023-05-21 15:36:02 +0200

    Update samostanERAdijagram.sql inserti

--- Vjezbe SQL sa ERA dijagramom/samostanERAdijagram/samostanERAdijagram.sql
+++ Vjezbe SQL sa ERA dijagramom/samostanERAdijagram/samostanERAdijagram.sql
@@ -42,3 +42,29 @@ create table svecenik_posao(
 alter table nadredjeni add foreign key (svecenik) references svecenik(id);
 alter table svecenik_posao add foreign key (posao) references posao(id);
 alter table svecenik_posao add foreign key (svecenik) references svecenik(id);
+
+
+
+insert into svecenik (ime,prezime) values('petar','martic');
+insert into svecenik (ime,prezime) values('luka','jelioc');
+insert into svecenik (ime,prezime) values('mario','brankoc');
+insert into svecenik (ime,prezime) values('luka','martic');
+
+
+insert into posao (trajanje,vrstaposla,zaduzenja) values('60','branjelisca','visezaduzenja');
+insert into posao (trajanje,vrstaposla,zaduzenja) values('60','skupljanjemseca','visezaduzenja');
+insert into posao (trajanje,vrstaposla,zaduzenja) values('60','pranjeoltara','jednozaduzenje');
+insert into posao (trajanje,vrstaposla,zaduzenja) values('60','posvecivanjejela','visezaduzenja');
+
+
+insert into svecenik_posao (svecenik,posao) values('1','2');
+insert into svecenik_posao (svecenik,posao) values('1','2');
+insert into svecenik_posao (svecenik,posao) values('1','2');
+insert into svecenik_posao (svecenik,posao) values('1','2');
+
+
+
+insert into nadredjeni (ime,prezime,oib,svecenik) values('mario','kraljic','08974859711','1');
+insert into nadredjeni (ime,prezime,oib,svecenik) values('marin','zaric','08659e59718','1');
+insert into nadredjeni (ime,prezime,oib,svecenik) values('petar','kojic','76739859732','1');
+insert into nadredjeni (ime,prezime,oib,svecenik) values('branimir','lukic','81748859718','1');

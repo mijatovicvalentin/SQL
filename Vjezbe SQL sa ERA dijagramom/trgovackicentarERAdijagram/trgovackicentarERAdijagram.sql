c254bb469b1f09e9d182f7e87ff8092d39093e1a
mijatovicvalentin <mijatovic.valentin123@gmail.com>
2023-05-21 16:07:32 +0200

    Update trgovackicentarERAdijagram.sql inserti

--- Vjezbe SQL sa ERA dijagramom/trgovackicentarERAdijagram/trgovackicentarERAdijagram.sql
+++ Vjezbe SQL sa ERA dijagramom/trgovackicentarERAdijagram/trgovackicentarERAdijagram.sql
@@ -40,3 +40,27 @@ create table sef(
 alter table trgovina_zaposlenik add foreign key (trgovina) references trgovina(id);
 alter table trgovina_zaposlenik add foreign key (zaposlenik) references zaposlenik(id);
 alter table sef add foreign key (zaposlenik) references zaposlenik(id);
+
+
+
+
+insert into trgovina (naziv,cijena,marka) values('sinsay','22.40','sinsay');
+insert into trgovina (naziv,cijena,marka) values('adidas','12.40','adidas');
+insert into trgovina (naziv,cijena,marka) values('nike','46.20','nike');
+insert into trgovina (naziv,cijena,marka) values('pullandbear','16.40','pullandbear');
+
+insert into zaposlenik (ime,prezime,oib) values('luka','martic','82738492819');
+insert into zaposlenik (ime,prezime,oib) values('marija','martinic','78245837481');
+insert into zaposlenik (ime,prezime,oib) values('lea','juros','98095897586');
+insert into zaposlenik (ime,prezime,oib) values('luka','ivanovic','82938209812');
+
+
+insert into trgovina_zaposlenik (trgovina,zaposlenik) values('1','2');
+insert into trgovina_zaposlenik (trgovina,zaposlenik) values('1','2');
+insert into trgovina_zaposlenik (trgovina,zaposlenik) values('1','2');
+insert into trgovina_zaposlenik (trgovina,zaposlenik) values('1','2');
+
+insert into sef (ime,prezime,odjeljenje,zaposlenik) values('martin','janjusic','odjeljenjejedan','1');
+insert into sef (ime,prezime,odjeljenje,zaposlenik) values('helena','lukic','odjeljenjejedan','1');
+insert into sef (ime,prezime,odjeljenje,zaposlenik) values('antonio','knezovic','odjeljenjejedan','1');
+insert into sef (ime,prezime,odjeljenje,zaposlenik) values('ena','lukic','odjeljenjejedan','1');

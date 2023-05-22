1d040b0d319827210d036c9bfa9a190b5e45d94c
mijatovicvalentin <mijatovic.valentin123@gmail.com>
2023-05-22 12:22:22 +0200

    Update urarsilvijaERAdijagram.sql inserti

--- Vjezbe SQL sa ERA dijagramom/urarsilvijaERAdijagram/urarsilvijaERAdijagram.sql
+++ Vjezbe SQL sa ERA dijagramom/urarsilvijaERAdijagram/urarsilvijaERAdijagram.sql
@@ -60,3 +60,40 @@ alter table sat_popravak add foreign key (sat) references sat(id);
 alter table sat_popravak add foreign key (popravak) references popravak(id);
 alter table popravak add foreign key (zaposlenik) references zaposlenik(id);
 alter table sat add foreign key (korisnik) references korisnik(id);
+
+
+
+
+insert into zaposlenik (ime,prezime,oib)  values('ivana','ivsic','98485739271');
+insert into zaposlenik (ime,prezime,oib)  values('marija','ivsic','73846839271');
+insert into zaposlenik (ime,prezime,oib)  values('ena','lukiic','09478645271');
+insert into zaposlenik (ime,prezime,oib)  values('dario','brajic','98265839271');
+
+insert into korisnik(ime,prezime,oib,kontakt) values('neven','maric','09272930582','gmail');
+insert into korisnik(ime,prezime,oib,kontakt) values('nevenka','milun','09274859182','gmail');
+insert into korisnik(ime,prezime,oib,kontakt) values('borna','marko','09827485182','gmail');
+insert into korisnik(ime,prezime,oib,kontakt) values('toni','taric','86474859182','gmail');
+
+
+
+insert into sat (korisnik,naziv,cijena,boja) values('1','markuff','12.60','bijela');
+insert into sat (korisnik,naziv,cijena,boja) values('1','baiollo','22.33','zuta');
+insert into sat (korisnik,naziv,cijena,boja) values('1','markuff','32.18','zelena');
+insert into sat (korisnik,naziv,cijena,boja) values('1','šerzoff','85.60','bijela');
+
+insert into popravak (cijena,trajanje,alat,zaposlenik) values('26.30','25','kljuc','1');
+insert into popravak (cijena,trajanje,alat,zaposlenik) values('33.30','40','kljuc','1');
+insert into popravak (cijena,trajanje,alat,zaposlenik) values('23.75','35','kljuc','1');
+insert into popravak (cijena,trajanje,alat,zaposlenik) values('17.30','20','kljuc','1');
+
+
+insert into sat_popravak (sat,popravak) values('1','2');
+insert into sat_popravak (sat,popravak) values('1','2');
+insert into sat_popravak (sat,popravak) values('1','2');
+insert into sat_popravak (sat,popravak) values('1','2');
+
+
+insert into strucnasprema (zaposlenik)  values('1');
+insert into strucnasprema (zaposlenik)  values('2');
+insert into strucnasprema (zaposlenik)  values('3');
+insert into strucnasprema (zaposlenik)  values('4');
